81cf8e54e61c8dbccd10ca05dd27c994dfe3f5e0
Gabriel Gonçalves Daltoso <ggdaltoso@gmail.com>
2015-03-22 03:13:00 -0300

    Add splash screen

--- proto/Project/app.js
+++ proto/Project/app.js
@@ -2,5 +2,10 @@ var app = angular.module( 'FramerApp', [ 'ngMaterial' ] );
 
 
 app.controller('FramerCtrl', ['$scope', function($scope){
-  
+  $scope.splahScreen = true;
+    
+    setTimeout(function(){
+        $scope.splahScreen = false;
+        $scope.$apply();
+    },3000)
 }]);
