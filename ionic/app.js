4413fddc105260a3b8fc96f712ad5f1ed0ac0b3a
ggdaltoso <gabriel.daltoso@GABRIELDALTOSO.opus.local>
2015-03-25 19:09:22 -0300

    Add icons

--- ionic/app.js
+++ ionic/app.js
@@ -1,11 +1,11 @@
 var app = angular.module( 'FramerApp', [ 'ngMaterial' ] );
 
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('red');
+  $mdThemingProvider.theme('default').backgroundPalette('red');
+})
 
 app.controller('FramerCtrl', ['$scope', function($scope){
   $scope.splahScreen = true;
-    
-    setTimeout(function(){
-        $scope.splahScreen = false;
-        $scope.$apply();
-    },3000)
 }]);
