fb793d0d50439fa10fde28c1a117c0b80e0b8dee
ggdaltoso <ggdaltoso@gmail.com>
2020-06-01 19:37:12 -0300

    add RecipeProvider to entire application

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import Tabletop from 'tabletop';
+import React, { useState } from 'react';
 import styled, { createGlobalStyle } from '@xstyled/styled-components';
-import { ThemeProvider, GlobalStyle, TaskBar, Alert } from '@react95/core';
-import { useSelector } from 'react-redux';
+import { ThemeProvider, GlobalStyle, TaskBar } from '@react95/core';
 
-import localforage from 'localforage';
-
-import {
-  Recipes,
-  Loading,
-  IngredientsModal,
-  RecipeModal,
-  TaskList,
-} from './components';
-
-const SPREADSHEET_ID = '1Uou8R5Bgrdl9M8ykKZeSj5MAl_huugiG3rRIQyMtxvI';
-
-const recipesDB = localforage.createInstance({
-  name: ' recipes',
-});
-
-const ingredientsDB = localforage.createInstance({
-  name: 'ingredients',
-});
+import { Recipes, IngredientsModal, RecipeModal, TaskList } from './components';
+import { useRecipes } from './components/RecipeContext';
 
 const isMobile =
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -42,13 +23,18 @@ const Style = createGlobalStyle`
 `;
 
 function App() {
-  const [recipes, setRecipes] = useState({});
-  const [allIngredients, setAllIngredients] = useState([]);
-  const [selectedRecipe, setSelectedRecipe] = useState({});
+  const {
+    recipes,
+    allIngredients,
+    selectedRecipe,
+    setSelectedRecipe,
+    setAllIngredients,
+    sheetId,
+    updateRecipes,
+  } = useRecipes();
+
   const [showModal, toggleModal] = useState(false);
-  const [loading, toggleLoading] = useState(false);
   const [showFilterModal, toggleFilterModal] = useState(false);
-  const [alertData, setAlertData] = useState(undefined);
 
   function openModal() {
     toggleModal(true);
@@ -57,121 +43,6 @@ function App() {
   function closeModal() {
     toggleModal(false);
   }
-
-  function getDataFromSpreadsheet() {
-    toggleLoading(true);
-    Tabletop.init({
-      key: SPREADSHEET_ID,
-      callback: (_, data) => {
-        const allRecipes = Object.values(data.models).map((m) => {
-          const pIndex = m.elements.findIndex((e) =>
-            e.Ingredientes.toLowerCase().includes('preparo'),
-          );
-          const imgIndex = m.elements.findIndex((e) =>
-            e.Ingredientes.toLowerCase().includes('imagens'),
-          );
-
-          const hasImgs = imgIndex !== -1;
-
-          const ingredients = m.elements.slice(0, pIndex);
-          const preparation = m.elements
-            .slice(pIndex + 1, hasImgs ? imgIndex : m.elements.length)
-            .map((i) => i.Ingredientes);
-
-          const images = hasImgs
-            ? m.elements
-                .slice(imgIndex + 1, m.elements.length)
-                .map((i) => i.Ingredientes)
-            : [];
-
-          return { name: m.name, ingredients, preparation, images };
-        });
-
-        const allIngredients = Array.from(
-          new Set(
-            allRecipes
-              .map((r) => r.ingredients.map((i) => i.Ingredientes))
-              .flat()
-              .sort(),
-          ),
-        ).map((i) => ({
-          name: i,
-          checked: false,
-        }));
-
-        recipesDB.setItem('recipes', allRecipes);
-        ingredientsDB.setItem('ingredients', allIngredients);
-
-        setRecipes(allRecipes);
-        setAllIngredients(allIngredients);
-
-        setTimeout(() => toggleLoading(false));
-      },
-      simpleSheet: true,
-    });
-  }
-
-  useEffect(() => {
-    async function fetchData() {
-      const recipes = await recipesDB.getItem('recipes');
-      const ingredients = await ingredientsDB.getItem('ingredients');
-
-      if (!recipes) {
-        getDataFromSpreadsheet();
-      } else {
-        setRecipes(recipes);
-        setAllIngredients(ingredients);
-      }
-    }
-
-    fetchData();
-  }, []);
-
-  const isServiceWorkerInitialized = useSelector(
-    (state) => state.serviceWorkerInitialized,
-  );
-  const isServiceWorkerUpdated = useSelector(
-    (state) => state.serviceWorkerUpdated,
-  );
-  const serviceWorkerRegistration = useSelector(
-    (state) => state.serviceWorkerRegistration,
-  );
-
-  useEffect(() => {
-    if (isServiceWorkerInitialized) {
-      setAlertData({
-        message: '95 Recipes has been saved for offline use.',
-        type: 'info',
-        title: 'Offile usage',
-        closeAlert: () => setAlertData(undefined),
-      });
-    }
-  }, [isServiceWorkerInitialized]);
-
-  useEffect(() => {
-    const updateServiceWorker = () => {
-      const registrationWaiting = serviceWorkerRegistration.waiting;
-
-      if (registrationWaiting) {
-        registrationWaiting.postMessage({ type: 'SKIP_WAITING' });
-
-        registrationWaiting.addEventListener('statechange', (e) => {
-          if (e.target.state === 'activated') {
-            window.location.reload();
-          }
-        });
-      }
-    };
-
-    if (isServiceWorkerUpdated) {
-      setAlertData({
-        message: 'New version available! Click here to update',
-        type: 'warning',
-        title: 'Update',
-        closeAlert: () => updateServiceWorker(),
-      });
-    }
-  }, [isServiceWorkerUpdated, serviceWorkerRegistration.waiting]);
 
   const filter = allIngredients.filter((t) => t.checked).map((i) => i.name);
 
@@ -209,20 +80,16 @@ function App() {
         />
       )}
 
-      {alertData && <Alert style={{ zIndex: 2 }} {...alertData} />}
-
       <TaskBar
         list={
           <TaskList
-            spreadsheetID={SPREADSHEET_ID}
+            spreadsheetID={sheetId}
             onUpdate={() => {
-              getDataFromSpreadsheet();
+              updateRecipes();
             }}
           />
         }
       />
-
-      {loading && <Loading />}
     </ThemeProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import configureStore from "./store";
-import { SW_INIT, SW_UPDATE } from "./types";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import configureStore from './store';
+import { SW_INIT, SW_UPDATE } from './types';
+import { RecipeProvider } from './components/RecipeContext';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <RecipeProvider>
+      <App />
+    </RecipeProvider>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 serviceWorker.register({
   onSuccess: () => store.dispatch({ type: SW_INIT }),
-  onUpdate: registration =>
-    store.dispatch({ type: SW_UPDATE, payload: registration })
+  onUpdate: (registration) =>
+    store.dispatch({ type: SW_UPDATE, payload: registration }),
 });
