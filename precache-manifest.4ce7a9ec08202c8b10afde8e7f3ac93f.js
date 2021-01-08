42178a9983d756887dcaf9b60bfca512c0203666
ggdaltoso <ggdaltoso@gmail.com>
2021-01-08 18:49:57 +0000

    Updates

---
+++ precache-manifest.4ce7a9ec08202c8b10afde8e7f3ac93f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244bba6cf7b201bca4b3d47113761c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d70b06430f6cce2e8b9",
+    "url": "/static/css/2.b198e031.chunk.css"
+  },
+  {
+    "revision": "7d70b06430f6cce2e8b9",
+    "url": "/static/js/2.87c47472.chunk.js"
+  },
+  {
+    "revision": "660cffbd5b3574cc33b895db57c047d5",
+    "url": "/static/js/2.87c47472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2201ba27ff9e65f5ff98",
+    "url": "/static/js/main.35093a30.chunk.js"
+  },
+  {
+    "revision": "f3eb6c251e76410c7879",
+    "url": "/static/js/runtime-main.113eafd7.js"
+  },
+  {
+    "revision": "7312851077a4e1a735a542d635e0997d",
+    "url": "/static/media/MS-Sans-Serif.73128510.eot"
+  },
+  {
+    "revision": "b57e2636989d2a69aae2e426f4f9aa49",
+    "url": "/static/media/MS-Sans-Serif.b57e2636.woff2"
+  },
+  {
+    "revision": "c44c956824147a532f6a7a1abdcf9433",
+    "url": "/static/media/MS-Sans-Serif.c44c9568.woff"
+  },
+  {
+    "revision": "f27c0623725cfeedb024d1883e5f1bce",
+    "url": "/static/media/MS-Sans-Serif.f27c0623.ttf"
+  },
+  {
+    "revision": "05f2a2b657ad94eb228ffc37cfaff08e",
+    "url": "/static/media/React95Video-Numbers.05f2a2b6.woff"
+  },
+  {
+    "revision": "10555f74d5fe75f3ce4fbdb66054c988",
+    "url": "/static/media/React95Video-Numbers.10555f74.woff2"
+  },
+  {
+    "revision": "76ac787871de3b943991c96a7a1c7d9d",
+    "url": "/static/media/React95Video-Numbers.76ac7878.eot"
+  },
+  {
+    "revision": "f6ed9192769e12940751ca69044f56b4",
+    "url": "/static/media/React95Video-Numbers.f6ed9192.ttf"
+  },
+  {
+    "revision": "29c235007007d931220e07475f3c0a88",
+    "url": "/static/media/check-disabled.29c23500.svg"
+  },
+  {
+    "revision": "8e6929ee77cc008e1e197e27f00c8339",
+    "url": "/static/media/check.8e6929ee.svg"
+  },
+  {
+    "revision": "821fd900fad1460afe35931e668c17a7",
+    "url": "/static/media/downcaret.821fd900.svg"
+  },
+  {
+    "revision": "cddcc886c9acb32f8222f2ecde723293",
+    "url": "/static/media/leftcaret.cddcc886.svg"
+  },
+  {
+    "revision": "274443ac2f681dd2045f40bef229492a",
+    "url": "/static/media/rightcaret.274443ac.svg"
+  },
+  {
+    "revision": "f1487e33c03c2b5ccfc5392896abe438",
+    "url": "/static/media/upcaret.f1487e33.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/95Recipes/precache-manifest.56d2e4fb8195871286bf7623bfaae370.js"
+  "/precache-manifest.4ce7a9ec08202c8b10afde8e7f3ac93f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/95Recipes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
